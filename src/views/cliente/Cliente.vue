01fcb8192776b2362fab2792f4fd45f3869e14aa
edisonGabrielFerrarini <egf22@hotmail.com>
2021-05-06 18:26:57 -0300

    ajuste

--- src/views/cliente/Cliente.vue
+++ src/views/cliente/Cliente.vue
@@ -1,7 +1,49 @@
 <template>
-  <main>
-    {{info}}
-  </main>
+  <v-main>
+    <v-app-bar 
+      app
+      dark
+    >
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+
+      <h1>{{info.saldo}}</h1>
+
+      <v-btn icon>
+        <v-icon
+          x-large
+        >mdi-diamond-stone</v-icon>
+      </v-btn>
+    <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      right
+      temporary
+    >
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <v-row>
+        <v-col>
+              <h1
+                class="titulo-ganhador"
+              ></h1>
+              <v-btn icon>
+                <v-icon
+                  x-large
+                >mdi-trophy</v-icon>
+              </v-btn>
+            </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-main>
 </template>
 
 <script>
@@ -10,7 +52,8 @@ import { getUser } from '../../services/login.service'
 export default {
   data(){
     return {
-      info: []
+      info: [],
+      drawer: null 
     }
   },
   created(){
@@ -26,5 +69,7 @@ export default {
 </script>
 
 <style>
-
+  .titulo-ganhador{
+    text-align: center;
+  }
 </style>
