238f63aee456977a6380ff5507a94d087c45964b
edisonGabrielFerrarini <egf22@hotmail.com>
2021-05-09 19:11:33 -0300

    modal ticket

--- src/components/cliente/cartela/ModalTicket.vue
+++ src/components/cliente/cartela/ModalTicket.vue
@@ -50,31 +50,91 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-snackbar
+      v-model="snackbar"
+      top
+    >
+      {{ text }}
+      
+      <template v-if="sucesso" v-slot:action="{ attrs }">
+        <v-btn
+          color="blue lighten-2"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          <v-icon>mdi-thumb-up</v-icon>
+        </v-btn>
+      </template>
+
+      <template v-else v-slot:action="{ attrs }">
+        <v-btn
+          color="red lighten-2"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          <v-icon>mdi-thumb-down</v-icon>
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-row>
 </template>
 
 <script>
 import { postTicket } from '../../../services/cartela.service'
-import {mapActions} from 'vuex'
+import {mapActions, mapGetters} from 'vuex'
+import { getCartela } from '../../../services/cartela.service'
  export default {
    props: {
     numerosEscolhidos: Array,
     id: Number,
     premio: Number,
-    idCliente: Number
+    idCliente: Number,
+    valor_bilhete: Number
    },
   data () {
     return {
+      snackbar: false,
+      sucesso: true,
       dialog: false,
+      text: '',
+      color: ''
     }
+  },
+  computed: {
+    ...mapGetters(['getInformacoes'])
   },
   methods: {
     ...mapActions(['updateSaldo']),
 
     async enviar(){
-      const response = await postTicket(this.idCliente, this.numerosEscolhidos)
-      this.updateSaldo(response.data.valor);
-      this.dialog = false
+      try{
+        if(this.getInformacoes.saldo >= this.valor_bilhete ){
+          const response = await postTicket(this.idCliente, this.numerosEscolhidos)
+          if(response.status === 202 || response.status === 200){
+            this.updateSaldo(response.data.valor);
+            this.dialog = false
+            this.snackbar = true
+            this.sucesso = true
+            this.text = 'Sua compra foi efetuada com sucesso'
+            this.color = 'success'
+          }
+        }else{
+            this.dialog = false
+            this.snackbar = true
+            this.sucesso = false
+            this.text = 'Seu saldo é insuficiente'
+            this.color = 'warning'
+        }
+      }catch(e){
+            this.dialog = false
+            this.snackbar = true
+            this.sucesso = false
+            this.text = 'Ocorreu um erro ao ralizar a compra'
+            this.color = 'warning'
+      }
+      
     }
   }
   }
